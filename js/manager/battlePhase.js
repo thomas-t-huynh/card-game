9b7fd423dbc8921a8f0709953b2ba76d85e2f307
unknown <t_huynh927@yahoo.com>
2024-03-02 20:05:48 -0800

    wip battlephase

--- js/manager/battlePhase.js
+++ js/manager/battlePhase.js
@@ -1,6 +1,45 @@
 class BattlePhase extends Phase {
-  constructor({ player, board, canvas }) {
-    super({ player, board, canvas });
-    this.name = 'Battle';
-  }
+	constructor({ state, board, canvas }) {
+		super({ state, board, canvas })
+		this.name = 'Battle'
+	}
+	addEventListeners() {
+		this.boundMouseMove = this.handleMouseMove.bind(this)
+		// this.boundMouseDown = this.handleMouseDown.bind(this)
+		this.boundEndButtonClick = this.handleEndPhase.bind(this)
+		this.canvas.addEventListener('mousemove', this.boundMouseMove)
+		// this.canvas.addEventListener('mousedown', this.boundMouseDown)
+		endButton.addEventListener('click', this.boundEndButtonClick)
+	}
+
+	removeEventListeners() {
+		this.canvas.removeEventListener('mousemove', this.boundMouseMove)
+		// this.canvas.removeEventListener('mousedown', this.boundMouseDown)
+		endButton.removeEventListener('click', this.boundEndButtonClick)
+	}
+
+	handleMouseMove(event) {
+		this.x = event.offsetX
+		this.y = event.offsetY
+		let somethingHovered = false
+		this.state.activePlayer.summons.cards.forEach((card) => {
+			if (
+				card.rectangle.getIsHover({ x: this.x, y: this.y }) &&
+				card.type === 'summon'
+			) {
+				card.rectangle.highlight = true
+				this.currentHover = card
+				somethingHovered = true
+			} else {
+				card.rectangle.highlight = false
+			}
+		})
+		if (!somethingHovered) {
+			this.currentHover = null
+		}
+	}
+
+	handleEndPhase() {
+		this.nextPhase()
+	}
 }
