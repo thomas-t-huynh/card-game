84705929c70f991c2895108b73b0eae67f1b72c0
thomas huynh <thomas.huynh@ruggable.com>
2024-03-27 12:41:25 -0700

    fixed up battle phase, deselect and select opposing player monster

--- js/manager/battlePhase.js
+++ js/manager/battlePhase.js
@@ -1,50 +1,58 @@
 class BattlePhase extends Phase {
-	constructor({ state, board, canvas }) {
-		super({ state, board, canvas })
-		this.name = 'Battle';
-		this.currentHover = null;
-		this.selectedIndex = -1;
-	}
-	addEventListeners() {
-		this.boundMouseMove = this.handleMouseMove.bind(this)
-		this.boundMouseDown = this.handleMouseDown.bind(this)
-		this.boundEndButtonClick = this.handleEndPhase.bind(this)
-		this.canvas.addEventListener('mousemove', this.boundMouseMove)
-		this.canvas.addEventListener('mousedown', this.boundMouseDown)
-		endButton.addEventListener('click', this.boundEndButtonClick)
-	}
+  constructor({ state, board, canvas }) {
+    super({ state, board, canvas });
+    this.name = 'Battle';
+    this.currentHover = null;
+    this.selectedIndex = -1;
+    this.currentHoverIndex = -1;
+  }
+  addEventListeners() {
+    this.boundMouseMove = this.handleMouseMove.bind(this);
+    this.boundMouseDown = this.handleMouseDown.bind(this);
+    this.boundEndButtonClick = this.handleEndPhase.bind(this);
+    this.canvas.addEventListener('mousemove', this.boundMouseMove);
+    this.canvas.addEventListener('mousedown', this.boundMouseDown);
+    endButton.addEventListener('click', this.boundEndButtonClick);
+  }
 
-	removeEventListeners() {
-		this.canvas.removeEventListener('mousemove', this.boundMouseMove)
-		this.canvas.removeEventListener('mousedown', this.boundMouseDown)
-		endButton.removeEventListener('click', this.boundEndButtonClick)
-	}
+  removeEventListeners() {
+    this.canvas.removeEventListener('mousemove', this.boundMouseMove);
+    this.canvas.removeEventListener('mousedown', this.boundMouseDown);
+    endButton.removeEventListener('click', this.boundEndButtonClick);
+  }
 
-	handleMouseMove(event) {
-		this.x = event.offsetX
-		this.y = event.offsetY
-		let somethingHovered = false
-		this.state.activePlayer.summons.cards.forEach((card, i) => {
-			if (
-				card.rectangle.getIsHover({ x: this.x, y: this.y }) &&
-				card.type === 'summon'
-			) {
-				card.rectangle.highlight = true
-				this.currentHover = card
-				somethingHovered = true
-				this.currentHoverIndex = i;
-			} else {
-				card.rectangle.highlight = false
-			}
-		})
-		if (!somethingHovered) {
-			this.currentHover = null;
-			this.currentHoverIndex = -1;
-		}
-	}
+  handleMouseMove(event) {
+    this.x = event.offsetX;
+    this.y = event.offsetY;
+    let somethingHovered = false;
+    const player = this.selectedCard
+      ? this.state.inactivePlayer
+      : this.state.activePlayer;
+    // hover over currently selected card
+    if (this.selectedCard?.rectangle?.getIsHover({ x: this.x, y: this.y })) {
+      this.currentHover = this.selectedCard;
+      return;
+    }
+    player.summons.cards.forEach((card, i) => {
+      if (
+        card.rectangle.getIsHover({ x: this.x, y: this.y }) &&
+        card.type === 'summon'
+      ) {
+        card.rectangle.highlight = true;
+        this.currentHover = card;
+        somethingHovered = true;
+        this.currentHoverIndex = i;
+      } else {
+        card.rectangle.highlight = false;
+      }
+    });
+    if (!somethingHovered) {
+      this.currentHover = null;
+      this.currentHoverIndex = -1;
+    }
+  }
 
   handleMouseDown(event) {
-    // play selected card on empty slot
     if (
       this.selectedCard &&
       this?.currentHover?.type === 'summon' &&
@@ -56,28 +64,32 @@ class BattlePhase extends Phase {
     // left mouse button click
     if (this.currentHover && event.button === 0) {
       if (this.selectedCard === this.currentHover) {
+        this.selectedCard.rectangle.highlight = false;
         this.selectedCard = null;
-				this.selectedIndex = -1;
+        this.selectedIndex = -1;
+        console.log('deselect');
       } else {
         this.selectedCard = this.currentHover;
-				this.selectedIndex = 
+        this.selectedIndex = this.currentHoverIndex;
       }
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.state.activePlayer.hand.setSelectedCard(this.selectedCard);
     }
   }
 
-	handleSummonBattle(enemySummon) {
-		if (enemySummon.attack > this.selectedCard.attack) {
-			this.state.activePlayer.summons[]
-		} else if (enemySummon.attack < this.selectedCard.attack) {
+  handleSummonBattle(enemySummon) {
+    console.log('BATTLE!');
+    if (enemySummon.attack > this.selectedCard.attack) {
+      // this.state.activePlayer.summons[]
+    } else if (enemySummon.attack < this.selectedCard.attack) {
+    } else {
+    }
+  }
 
-		} else {
-
-		}
-	}
-
-	handleEndPhase() {
-		this.nextPhase()
-	}
+  handleEndPhase() {
+    this.currentHover = null;
+    this.selectedIndex = -1;
+    this.currentHoverIndex = -1;
+    this.nextPhase();
+  }
 }
--- js/manager/manager.js
+++ js/manager/manager.js
@@ -1,121 +1,121 @@
 class Manager {
-	constructor({ players, board, canvas }) {
-		this.players = players
-		// TODO - replease w/ an array of phase classes?
-		this.board = board
-		this.canvas = canvas
-		this.ctx = canvas.getContext('2d')
-		this.x = 0
-		this.y = 0
-		this.currentHover = null
-		this.selectedCard = null
-		this.selectedCards = []
-		this.selectedSlot = -1
-		this.state = {
-			hasSummoned: false,
-			activePlayer: players[0],
-			inactivePlayer: players[1],
-			firstTurn: true,
-			setHasSummoned(value) {
-				this.hasSummoned = value
-			},
-			resetState() {
-				this.hasSummoned = false
-				const temp = this.activePlayer
-				this.activePlayer = this.inactivePlayer
-				this.inactivePlayer = temp
-				this.firstTurn = false
-			},
-		}
+  constructor({ players, board, canvas }) {
+    this.players = players;
+    // TODO - replease w/ an array of phase classes?
+    this.board = board;
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.x = 0;
+    this.y = 0;
+    this.currentHover = null;
+    this.selectedCard = null;
+    this.selectedCards = [];
+    this.selectedSlot = -1;
+    this.state = {
+      hasSummoned: false,
+      activePlayer: players[0],
+      inactivePlayer: players[1],
+      firstTurn: true,
+      setHasSummoned(value) {
+        this.hasSummoned = value;
+      },
+      resetState() {
+        this.hasSummoned = false;
+        const temp = this.activePlayer;
+        this.activePlayer = this.inactivePlayer;
+        this.inactivePlayer = temp;
+        this.firstTurn = false;
+      },
+    };
 
-		const drawPhase = new DrawPhase({
-			state: this.state,
-			board,
-			canvas,
-		})
-		const standyByPhase = new StandByPhase({
-			state: this.state,
-			board,
-			canvas,
-		})
-		const mainPhase1 = new MainPhase({
-			state: this.state,
-			board,
-			canvas,
-		})
-		const battlePhase = new BattlePhase({
-			state: this.state,
-			board,
-			canvas,
-		})
-		// this.phases = ['draw', 'standBy', 'main1', 'battle', 'main2', 'end'];
-		this.phases = [drawPhase, standyByPhase, mainPhase1, battlePhase]
-		this.currentPhase = 0
+    const drawPhase = new DrawPhase({
+      state: this.state,
+      board,
+      canvas,
+    });
+    const standyByPhase = new StandByPhase({
+      state: this.state,
+      board,
+      canvas,
+    });
+    const mainPhase1 = new MainPhase({
+      state: this.state,
+      board,
+      canvas,
+    });
+    const battlePhase = new BattlePhase({
+      state: this.state,
+      board,
+      canvas,
+    });
+    // this.phases = ['draw', 'standBy', 'main1', 'battle', 'main2', 'end'];
+    this.phases = [drawPhase, standyByPhase, mainPhase1, battlePhase];
+    this.currentPhase = 0;
 
-		this.assignNextPhases()
+    this.assignNextPhases();
 
-		this.boundHandleMouseMove = this.handleMouseMove.bind(this)
-		this.canvas.addEventListener('mousemove', this.boundHandleMouseMove)
-	}
+    this.boundHandleMouseMove = this.handleMouseMove.bind(this);
+    this.canvas.addEventListener('mousemove', this.boundHandleMouseMove);
+  }
 
-	getCurrentPhaseName() {
-		return this.phases[this.currentPhase].name
-	}
+  getCurrentPhaseName() {
+    return this.phases[this.currentPhase].name;
+  }
 
-	activateCurrentPhase() {
-		this.phases[this.currentPhase].addEventListeners()
-	}
+  activateCurrentPhase() {
+    this.phases[this.currentPhase].addEventListeners();
+  }
 
-	deactivateCurrentPhase() {
-		this.phases[this.currentPhase].removeEventListeners()
-	}
+  deactivateCurrentPhase() {
+    this.phases[this.currentPhase].removeEventListeners();
+  }
 
-	nextPhase() {
-		this.deactivateCurrentPhase()
-		this.currentPhase = (this.currentPhase + 1) % this.phases.length
-		if (this.currentPhase === 0) {
-			this.state.resetState()
-		}
-		this.activateCurrentPhase()
-	}
+  nextPhase() {
+    this.deactivateCurrentPhase();
+    this.currentPhase = (this.currentPhase + 1) % this.phases.length;
+    if (this.currentPhase === 0) {
+      this.state.resetState();
+    }
+    this.activateCurrentPhase();
+  }
 
-	assignNextPhases() {
-		this.boundNextPhase = this.nextPhase.bind(this)
-		this.phases.forEach((phase) => phase.setNextPhase(this.boundNextPhase))
-	}
+  assignNextPhases() {
+    this.boundNextPhase = this.nextPhase.bind(this);
+    this.phases.forEach((phase) => phase.setNextPhase(this.boundNextPhase));
+  }
 
-	//* old drag and drop - maybe there will be a use for it someday.
-	handleMouseMove(event) {
-		this.x = event.offsetX
-		this.y = event.offsetY
-		this.currentHover = null
-		this.state.activePlayer.hand.cards.forEach(this.handleSetCurrentHover)
-		this.board.frontPlayer.summons.cards.forEach(this.handleSetCurrentHover)
-		this.board.frontPlayer.tactics.cards.forEach(this.handleSetCurrentHover)
-		this.board.backPlayer.summons.cards.forEach(this.handleSetCurrentHover)
-		this.board.backPlayer.tactics.cards.forEach(this.handleSetCurrentHover)
-		if (!this.currentHover) {
-			cardInfoUi.innerText = ''
-		}
-	}
+  //* old drag and drop - maybe there will be a use for it someday.
+  handleMouseMove(event) {
+    this.x = event.offsetX;
+    this.y = event.offsetY;
+    this.currentHover = null;
+    this.state.activePlayer.hand.cards.forEach(this.handleSetCurrentHover);
+    this.board.frontPlayer.summons.cards.forEach(this.handleSetCurrentHover);
+    this.board.frontPlayer.tactics.cards.forEach(this.handleSetCurrentHover);
+    this.board.backPlayer.summons.cards.forEach(this.handleSetCurrentHover);
+    this.board.backPlayer.tactics.cards.forEach(this.handleSetCurrentHover);
+    if (!this.currentHover) {
+      cardInfoUi.innerText = '';
+    }
+  }
 
-	handleSetCurrentHover = (card) => {
-		if (
-			card.rectangle.getIsHover({ x: this.x, y: this.y }) &&
-			card.type !== 'empty'
-		) {
-			this.currentHover = card
-			cardInfoUi.innerText = `type: ${this.currentHover.type},  name: ${this.currentHover.name}`
-		}
-	}
+  handleSetCurrentHover = (card) => {
+    if (
+      card.rectangle.getIsHover({ x: this.x, y: this.y }) &&
+      card.type !== 'empty'
+    ) {
+      this.currentHover = card;
+      cardInfoUi.innerText = `type: ${this.currentHover.type},  name: ${this.currentHover.name}`;
+    }
+  };
 
-	// handleMouseDown() {
-	//   if (this.onDrag) {
-	//     this.onDrag = false;
-	//     return;
-	//   }
-	//   if (this.currentHover) {
-	//     this.onDrag = true;
-	//   }
-	// }
+  // handleMouseDown() {
+  //   if (this.onDrag) {
+  //     this.onDrag = false;
+  //     return;
+  //   }
+  //   if (this.currentHover) {
+  //     this.onDrag = true;
+  //   }
+  // }
 }
